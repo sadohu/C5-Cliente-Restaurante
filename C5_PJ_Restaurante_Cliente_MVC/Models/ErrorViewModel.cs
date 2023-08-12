5ac81d45909d24abbe08fdb597c82582dbeb1639
sadohu <sadohu26@yahoo.com>
2023-08-12 12:55:09 -0500

    Agregar archivos de proyecto.

---
+++ C5_PJ_Restaurante_Cliente_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace C5_PJ_Restaurante_Cliente_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
